50ed695b77ad7ed8aa9c51d287b809297324d587
dev-py-me <140444355+dev-py-me@users.noreply.github.com>
2024-01-12 12:49:46 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 # Pyrogram Client Arguments
-API_ID = ''
-API_HASH = ''
-BOT_TOKEN = ''
+API_ID = '7712824'
+API_HASH = '2d3673e18b462f8032c4eea2f50b9f52'
+BOT_TOKEN = '1832176378:AAGhfQbgbTOo6id__4-OFDKz-PpmBJ34SS8'
 
 # Bot's Functionality Parameters
 MAX_FILE_SIZE = 2e8
